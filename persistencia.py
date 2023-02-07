57faac571700d3b2723b9baaa348f184aef3c819
Sergio Sevilla <bitgenius@hotmail.com>
2023-02-07 21:34:20 +0100

    Añadir información de tablas para crear BD desde YAML Inicialización de Base de datos en SQLITE3.

--- persistencia.py
+++ persistencia.py
@@ -5,14 +5,32 @@ from sqlite3 import Error
 class Persistencia:
     def __init__(self,configuracion):
         database_path = configuracion["basedatos"]["path"]
-        if not os.path.isfile(database_path):
-            print("no existe base datos "+database_path+". Inicializando...")
-            try:
-                conn = sqlite3.connect(database_path)
-            except Error as e:
-                print(e)
-        else:
-            print("Base de datos encontrada.")
-            #Check tables and create
+        self.conexion = sqlite3.connect(database_path)
+        self.crear_esquema(configuracion)
+        if self.check_productos() == 0:
+            self.inicializar_productos()
 
 
+    def crear_esquema (self,configuracion):
+        c = self.conexion.cursor()
+        for tabla in configuracion["tables"]:
+            create_str = "CREATE TABLE IF NOT EXISTS "
+            create_str = create_str + tabla + "("
+            columnas = configuracion["tables"][tabla]["columns"]
+            first = True
+            for atributo in columnas:
+                if not first : create_str = create_str+", "
+                create_str = create_str + "[" + atributo + "] " +configuracion["tables"][tabla]["columns"][atributo]["type"]
+                first = False
+            create_str = create_str + ")"
+            c.execute(create_str)
+
+    def check_productos(self):
+        cursor = self.conexion.cursor()
+        cursor.execute("SELECT * FROM productos")
+        results = cursor.fetchall()
+        return len (results)
+
+    def inicializar_productos(self):
+        #todo: llamar a la API de almacen para poblar la tabla de productos
+        print("Consultando almacen...")
